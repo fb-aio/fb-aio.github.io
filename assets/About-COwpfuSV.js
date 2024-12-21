8694c2ea9fd1ac0d0806be75d36177e076b0117c
hoang.tran12 <99.hoangtran@gmail.com>
2024-12-22 02:57:05 +0700

    auto deploy

--- assets/About-COwpfuSV.js
+++
@@ -1,1 +0,0 @@
-import{r as u,aK as o}from"./index-BoZ3qCu-.js";import{i as s,a5 as n}from"./MyApp-D00-hVKM.js";function l({target:i}){return s("About."+(i==null?void 0:i.id),null),u.useEffect(()=>{i!=null&&i.id&&n(i.id)},[i==null?void 0:i.id]),o.jsx(o.Fragment,{children:"About"})}export{l as default};
