0aa62094db9d492573b790ac9944af0934200d44
hoang.tran12 <99.hoangtran@gmail.com>
2024-12-23 01:37:00 +0700

    auto deploy

---
+++ assets/About-Dboyj3Ka.js
@@ -0,0 +1,1 @@
+import{r as u,aK as o}from"./index-CvQtIRe7.js";import{i as s,a5 as n}from"./MyApp-DyMot7yF.js";function l({target:i}){return s("About."+(i==null?void 0:i.id),null),u.useEffect(()=>{i!=null&&i.id&&n(i.id)},[i==null?void 0:i.id]),o.jsx(o.Fragment,{children:"About"})}export{l as default};
--- assets/GetAccessTokenBtn-BxueealB.js
+++
@@ -1,9 +0,0 @@
-import{aI as m,aK as n,aM as p}from"./index-BktjDUXC.js";import{u,A as h,B as y,t as a,e as l}from"./MyApp-C4LknYZT.js";import{D as d}from"./index-C1pwPlvb.js";import"./Dropdown-ewQssvmI.js";import"./PurePanel-DwFKBgmy.js";function b(){const{ti:o}=m(),{message:s,notification:r}=u(),k=async e=>{if(e.key==="check-permission"){a("GetAccessTokenBtn:checkPermission");const i=prompt(o({en:`Enter access token that you want to check in here:
-
-+ Check token validity
-+ Check token permissions
-+ Check token expiration time`,vi:`Nhập access token bạn muốn kiểm tra vào đây:
-
-+ Kiểm tra token hợp lệ hay không
-+ Kiểm tra các quyền token có thể làm
-+ Kiểm tra thời gian hết hạn`}),"");i&&window.open("https://developers.facebook.com/tools/debug/accesstoken/?access_token="+i);return}const t="GetAccessTokenBtn:"+e.key;a(t),s.loading({key:t,content:`Getting access token ${e.key}...`,duration:0});const c=await l(e.key,!0,!0);c?(s.destroy(t),r.open({type:"success",message:`Access token ${e.key}`,description:c})):s.error({key:t,content:`Cannot get access token ${e.key}`})};return n.jsx(d,{menu:{items:[{key:"title",label:"Get access token",type:"group"},{type:"divider"},...Object.entries(h).map(([e,t])=>({key:e,label:"🔑 "+t})),{type:"divider"},{key:"check-permission",label:"🔎 "+o({en:"Check token",vi:"Kiểm tra token"})}],onClick:k},arrow:!0,children:n.jsx(y,{dot:!0,children:n.jsx(p,{icon:n.jsx("i",{className:"fa-solid fa-key"})})})})}export{b as default};
