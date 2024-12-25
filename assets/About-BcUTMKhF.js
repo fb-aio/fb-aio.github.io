7d84a3a76c0d0688f24afb55d97a58ea99f2e252
hoang.tran12 <99.hoangtran@gmail.com>
2024-12-26 01:09:03 +0700

    auto deploy

---
+++ assets/About-BcUTMKhF.js
@@ -0,0 +1,1 @@
+import{r as u,aK as o}from"./index-Cf3ye8Qe.js";import{i as s,a5 as n}from"./MyApp-Cct2xRbp.js";function l({target:i}){return s("About."+(i==null?void 0:i.id),null),u.useEffect(()=>{i!=null&&i.id&&n(i.id)},[i==null?void 0:i.id]),o.jsx(o.Fragment,{children:"About"})}export{l as default};
---
+++ assets/index-C_Z8SfOO.js
@@ -0,0 +1,1 @@
+import{bT as r,bU as o}from"./index-Cf3ye8Qe.js";const a=r;a.ErrorBoundary=o;export{a as A};
