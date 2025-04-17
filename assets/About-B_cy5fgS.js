fa298739cc251bfca4febe3f63240178acfd957e
hoang.tran12 <99.hoangtran@gmail.com>
2025-04-18 00:37:48 +0700

    auto deploy

---
+++ assets/About-B_cy5fgS.js
@@ -0,0 +1,1 @@
+import{r as u,b8 as o}from"./index-DZ0t5pqt.js";import{i as s,aH as n}from"./MyApp-DGIdqL5G.js";function l({target:i}){return s("About."+(i==null?void 0:i.id),null),u.useEffect(()=>{i!=null&&i.id&&n(i.id)},[i==null?void 0:i.id]),o.jsx(o.Fragment,{children:"About"})}export{l as default};
--- assets/Share-DkxVtrnH.js
+++
@@ -1,1 +0,0 @@
-import{b8 as r}from"./index-D3GSlXte.js";function o({target:t,postId:e}){return r.jsx("div",{children:"Share"})}export{o as default};
