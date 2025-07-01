31937b870ae468cfd3cc2cf28bbd9e6d77285bea
hoang.tran12 <99.hoangtran@gmail.com>
2025-07-01 14:36:05 +0700

    auto deploy

--- assets/Friends-C2oRwDPN.js
+++
@@ -1,1 +0,0 @@
-import{r,b3 as t}from"./index-CfQoHRTF.js";import{u as s}from"./MyApp-Cmr5dNjq.js";function e({target:e}){const{ti:i}=s(),[n,o]=r.useState([]);return t.jsx("div",{children:t.jsx("h1",{children:i({en:"🚧 Work in progress",vi:"🚧 Đang phát triển"})})})}export{e as default};
