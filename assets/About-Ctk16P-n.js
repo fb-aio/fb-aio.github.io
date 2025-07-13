118dc9af584c708b9bb7b8625555fe8dd7a79497
hoang.tran12 <99.hoangtran@gmail.com>
2025-07-13 23:55:27 +0700

    auto deploy

---
+++ assets/About-Ctk16P-n.js
@@ -0,0 +1,1 @@
+import{r as t,b0 as i}from"./index-DFGxtoEp.js";import o from"./useCacheState-V3fHLBe9.js";import{aP as r}from"./MyApp-BCtP09Il.js";function e({target:e}){return o("About."+(null==e?void 0:e.id),null),t.useEffect((()=>{(null==e?void 0:e.id)&&r(e.id)}),[null==e?void 0:e.id]),i.jsx(i.Fragment,{children:"About"})}export{e as default};
