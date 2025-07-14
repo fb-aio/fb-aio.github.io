10954b794d8c12d3b850738f5bf16889138a0836
hoang.tran12 <99.hoangtran@gmail.com>
2025-07-14 13:17:38 +0700

    auto deploy

--- assets/Friends-baj5xkA7.js
+++
@@ -1,1 +0,0 @@
-import{r,b0 as t}from"./index-Cj-3AQ62.js";import{u as s}from"./MyApp-K6L0K6_y.js";function e({target:e}){const{ti:i}=s(),[n,o]=r.useState([]);return t.jsx("div",{children:t.jsx("h1",{children:i({en:"🚧 Work in progress",vi:"🚧 Đang phát triển"})})})}export{e as default};
