efdfd57ea5884db7354a1a16fd6ecd1d17173571
Hoang Tran - mac pro <99.hoangtran@gmail.com>
2025-08-07 18:09:38 +0700

    auto deploy

--- assets/Friends-D0FucCLr.js
+++
@@ -1,1 +0,0 @@
-import{r,b1 as t}from"./index-CbZdDnHV.js";import{u as s}from"./MyApp-Bta4pt8z.js";function e({target:e}){const{ti:i}=s(),[n,o]=r.useState([]);return t.jsx("div",{children:t.jsx("h1",{children:i({en:"🚧 Work in progress",vi:"🚧 Đang phát triển"})})})}export{e as default};
