18cf55604e8e05834024e5b2bc5df9517b9736fe
Hoang Tran - mac pro <99.hoangtran@gmail.com>
2025-09-18 15:10:47 +0700

    auto deploy

---
+++ assets/About-BSOlT-W1.js
@@ -0,0 +1,1 @@
+import{r as t,b1 as i}from"./index-DloSTllm.js";import o from"./useCacheState-DKIBP1xh.js";import{aU as r}from"./MyApp-CCg7dWii.js";function e({target:e}){return o("About."+(null==e?void 0:e.id),null),t.useEffect((()=>{(null==e?void 0:e.id)&&r(e.id)}),[null==e?void 0:e.id]),i.jsx(i.Fragment,{children:"About"})}export{e as default};
